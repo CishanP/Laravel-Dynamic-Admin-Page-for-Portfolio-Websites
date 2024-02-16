e4ce2e146043bceb8d88058caa55e12a45b2fdb0
Ishan Pandit <121755556+CishanP@users.noreply.github.com>
2024-02-16 19:09:46 +0545

    Add files via upload

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AdminController extends Controller
+{
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        $notification = array(
+            'message' => 'User Logged Out',
+            'alert-type' => 'info'
+        );
+        return redirect('/login')->with($notification);
+    }
+
+    public function Profile() {
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin_profile_view', compact('adminData'));
+    }
+
+    public function EditProfile() {
+        $id = Auth::user()->id;
+        $editData = User::find($id);
+        return view('admin.admin_profile_edit', compact('editData'));
+    }
+
+    public function StoreProfile(Request $request) {
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->email = $request->email;
+
+        if($request->file('profile_image')) {
+            $file = $request->file('profile_image');
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'), $filename);
+            $data['profile_image'] = $filename;
+        }
+        $data->save();
+        $notification = array(
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('admin.profile')->with($notification);
+    }
+}
